64c6d16e0b5cc387174e7eacec489d5a62d6bad4
VeniceSandie <sandisiwe2405@gmail.com>
2022-11-29 12:43:11 +0000

    revised task

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{:s}{:s}{:s}".format(str, str, str))
-print("{:.9s}".format(str))
+_str = str[0:9]
+print('{:s}{:s}{:s}\n{:s}'.format(str, str, str, _str))
